13e55f7021f1d83f555df14471807d723f6e2763
golabm1993 <golabm236@gmail.com>
2019-04-30 09:25:42 +0200

    Family member CRUD - review

--- src/main/java/pl/golabm/repository/PersonRepository.java
+++
@@ -1,7 +0,0 @@
-package pl.golabm.repository;
-
-import org.springframework.data.repository.CrudRepository;
-import pl.golabm.model.Person;
-
-public interface PersonRepository extends CrudRepository<Person, Long> {
-}
